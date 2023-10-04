bee07c4e5fd71906175fbf45f5cf4e655723b998
Руслан <@DESKTOP-5PQRGGU>
2023-10-04 14:13:05 +0600

    Добавьте файлы проекта.

---
+++ Numbers/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LowBor = new System.Windows.Forms.TextBox();
+            this.HighBor = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.Numb = new System.Windows.Forms.ListBox();
+            this.Fibon = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnFib = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "To";
+            // 
+            // LowBor
+            // 
+            this.LowBor.Location = new System.Drawing.Point(143, 89);
+            this.LowBor.Name = "LowBor";
+            this.LowBor.Size = new System.Drawing.Size(100, 20);
+            this.LowBor.TabIndex = 2;
+            this.LowBor.TextChanged += new System.EventHandler(this.LowBorder_TextChanged);
+            // 
+            // HighBor
+            // 
+            this.HighBor.Location = new System.Drawing.Point(143, 162);
+            this.HighBor.Name = "HighBor";
+            this.HighBor.Size = new System.Drawing.Size(100, 20);
+            this.HighBor.TabIndex = 3;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(306, 321);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(52, 321);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Numbers";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Numb
+            // 
+            this.Numb.FormattingEnabled = true;
+            this.Numb.Location = new System.Drawing.Point(328, 38);
+            this.Numb.Name = "Numb";
+            this.Numb.Size = new System.Drawing.Size(120, 95);
+            this.Numb.TabIndex = 6;
+            // 
+            // Fibon
+            // 
+            this.Fibon.FormattingEnabled = true;
+            this.Fibon.Location = new System.Drawing.Point(328, 190);
+            this.Fibon.Name = "Fibon";
+            this.Fibon.Size = new System.Drawing.Size(120, 95);
+            this.Fibon.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(328, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Digits";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(328, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Fibonachi";
+            // 
+            // btnFib
+            // 
+            this.btnFib.Location = new System.Drawing.Point(177, 321);
+            this.btnFib.Name = "btnFib";
+            this.btnFib.Size = new System.Drawing.Size(75, 23);
+            this.btnFib.TabIndex = 10;
+            this.btnFib.Text = "Fibonachi";
+            this.btnFib.UseVisualStyleBackColor = true;
+            this.btnFib.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 405);
+            this.Controls.Add(this.btnFib);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Fibon);
+            this.Controls.Add(this.Numb);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.HighBor);
+            this.Controls.Add(this.LowBor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LowBor;
+        private System.Windows.Forms.TextBox HighBor;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox Numb;
+        private System.Windows.Forms.ListBox Fibon;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnFib;
+    }
+}
+
---
+++ Numbers/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Numbers
+{
+    public partial class Form1 : Form
+    {
+        public int lowBord;
+        public int? highBord;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(PrintN);
+            th.IsBackground = true;
+            th.Start();
+
+        }
+
+
+        public void PrintN()
+        {
+
+            lowBord = 2;
+            highBord = null;
+
+            if (!string.IsNullOrEmpty(LowBor.Text))
+            {
+                int.TryParse(LowBor.Text, out lowBord);
+            }
+
+            if (!string.IsNullOrEmpty(HighBor.Text))
+            {
+                int.TryParse(HighBor.Text, out int val);
+                highBord = val;
+            }
+
+
+            for (int num = lowBord; num <= highBord; num++)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    Numb.Items.Add(num);
+                });
+            }
+
+        }
+
+        public void PrintF()
+        {
+            int N1 = 1;
+            int N2 = 2;
+            for(int i = 0; i < highBord; i++)
+            {
+                var N3 = N1 + N2;
+                N1 = N2;
+                N2 = N3;
+
+                this.Invoke((MethodInvoker)delegate
+                {
+                    Fibon.Items.Add(N1);
+                });
+            }
+        }
+  
+
+        private void LowBorder_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            Thread th2 = new Thread(PrintF);
+            th2.IsBackground = true;
+            th2.Start();
+        }
+    }
+}
